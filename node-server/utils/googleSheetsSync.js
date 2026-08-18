263b85c5e85f417bafef1452998c1de61fd20f38
AstroDrip <jasil001grim@gmail.com>
2026-08-19 02:45:38 +0530

    Update googleSheetsSync.js

--- node-server/utils/googleSheetsSync.js
+++ node-server/utils/googleSheetsSync.js
@@ -39,7 +39,16 @@ async function pushOrderRowsToSheet(rows, sheetName = "Orders") {
     values.push([
       r.invoice_id,
       r.order_id,
-      new Date(r.created_at).toISOString(),
+      new Date(r.created_at).toLocaleString("en-IN", {
+    timeZone: "Asia/Kolkata",
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+  }),
       r.name,
       r.phone,
       r.total,
